3a87286ac4c5b226f6c1b164d0b108f48bfa7a78
catuxian <15926gg@gmail.com>
2020-11-12 16:29:41 +0800

    將pdo連線移出至base.php檔案

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -2,17 +2,15 @@
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
 
-$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
-$pdo=new PDO($dsn,'root','');
+include_once "../base.php";
 
 echo "<pre>";
 print_r(array_keys($_POST));
 echo "</pre>";
 
-
 $sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values('".implode("','",$_POST)."')";
-echo $sql;
-$pdo->exec($sql);
+
+$pdo->query($sql);
 
 header("location:../index.php");
 
