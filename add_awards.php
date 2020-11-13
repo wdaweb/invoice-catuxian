30d891fc2b29a6f2826cd3a36d41d8e67c612101
catuxian <15926gg@gmail.com>
2020-11-13 15:01:46 +0800

    完成新增各期開獎獎號紀錄功能

--- add_awards.php
+++ add_awards.php
@@ -27,22 +27,22 @@
         <tr>
             <th id="grandPrize" rowspan="2">特獎</th>
             <td headers="grandPrize" class="number">
-                <input type="text">
+                <input type="text" name="grand_prize" min="00000001" max="99999999">
             </td>
         </tr>
         <tr>
             <td headers="grandPrize"> 同期統一發票收執聯8位數號碼與特獎號碼相同者獎金200萬元 </td>
         </tr>
         <tr>
-            <th id="firstPrize" rowspan="2">頭獎</th>
-            <td headers="firstPrize" class="number">
-                <input type="number" name="first_price[]" min="00000001" max="99999999">
-                <input type="number" name="first_price[]" min="00000001" max="99999999">
-                <input type="number" name="first_price[]" min="00000001" max="99999999">
+            <th id="first_prize" rowspan="2">頭獎</th>
+            <td headers="first_prize" class="number">
+                <input type="number" name="first_prize[]" min="00000001" max="99999999">
+                <input type="number" name="first_prize[]" min="00000001" max="99999999">
+                <input type="number" name="first_prize[]" min="00000001" max="99999999">
             </td>
         </tr>
         <tr>
-            <td headers="firstPrize"> 同期統一發票收執聯8位數號碼與頭獎號碼相同者獎金20萬元 </td>
+            <td headers="first_prize"> 同期統一發票收執聯8位數號碼與頭獎號碼相同者獎金20萬元 </td>
         </tr>
         <tr>
             <th id="twoPrize">二獎</th>
