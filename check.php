290c9ae1bb68410bf8db7966b467925ab8d1a07d
catuxian <15926gg@gmail.com>
2020-11-26 23:00:01 +0800

    新增登入判斷

--- check.php
+++ check.php
@@ -1,4 +1,11 @@
 <?php
     //檢查登入資訊並導向default.php頁面
-    header("location:default.php");
+    print_r($_POST);
+    
+    if($_POST['acc']=='jack'&&$_POST['pw']=='1234'){
+        echo "登入成功";
+        header("location:default.php");
+    }else{
+        echo "登入失敗";
+    }
 ?>
--- index.php
+++ index.php
@@ -3,12 +3,12 @@ include "header.php";
 ?>
 <h3 class="text-center">登入頁面</h3>
 <div class="container text-center login">
-    <form action="check.php">
+    <form action="check.php" method="post">
         <div class="my-3">
-            帳號：<input type="acc">
+            帳號：<input type="acc" name="acc">
         </div>
         <div class="my-3">
-            密碼：<input type="password">
+            密碼：<input type="password" name="pw">
         </div>
         <input type="submit">
     </form>
