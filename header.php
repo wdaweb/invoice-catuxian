21e1472966852c775dd051d27af2fe03a7346373
catuxian <15926gg@gmail.com>
2020-11-27 22:15:24 +0800

    修改登入頁面的樣式

--- header.php
+++ header.php
@@ -2,12 +2,15 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>統一發票紀錄及對獎系統</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="custom.css">
 </head>
+
 <body>
--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
 include "header.php";
 ?>
-<h3 class="text-center">登入頁面</h3>
-<div class="container text-center login">
-    <form action="check.php" method="post">
-        <div class="tip">
-        <?php
-            if(isset($_GET['false'])){
-                echo "帳號或密碼錯誤";
-            }
-        ?>
-        </div>
-        <div class="my-3">
-            帳號：<input type="acc" name="acc">
-        </div>
-        <div class="my-3">
-            密碼：<input type="password" name="pw">
-        </div>
-        <input type="submit">
-    </form>
+<div class="container text-center login my-5 py-5 col-6 animate__animated animate__fadeInDown row mx-auto">
+    <h3 class="text-center col-12">登入頁面</h3>
+        <form action="check.php" method="post" class="col-12">
+            <div class="tip">
+            <?php
+                if(isset($_GET['false'])){
+                    echo "帳號或密碼錯誤";
+                }
+            ?>
+            </div>
+            <div class="my-3">
+                <div class="mb-3">帳號：</div>
+                <input class="col-6" type="acc" name="acc" placeholder="rose">                
+            </div>
+            <div class="my-3">
+                <div class="mb-3">密碼：</div>
+                <input class="col-6" type="password" name="pw" placeholder="1234">
+            </div>
+            <input type="submit" value="登入">
+        </form>
 </div>
 <?php
     include "footer.php";
