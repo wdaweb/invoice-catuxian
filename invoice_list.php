04a4a73681f0fdd04524c4c09e61165a73ddf8cf
catuxian <15926gg@gmail.com>
2020-12-01 20:24:48 +0800

    修改首頁換頁無法排序問題

--- invoice_list.php
+++ invoice_list.php
@@ -1,41 +1,84 @@
 <?php
 include_once "base.php";
-//以網址收到的參數判斷要顯示何時的發票以及依照甚麼排序
-if(isset($_GET['pd'])&&isset($_GET['orderby'])){
-    $year=explode("-",$_GET['pd'])[0];
-    $period=explode("-",$_GET['pd'])[1];
-    switch ($_GET['orderby']){
-        case "date":
-            $rows=$pdo->query("select * from `invoices` where period={$period} order by date desc")->fetchAll();
-        break;
-        case "payment":
-            $rows=$pdo->query("select * from `invoices` where period={$period} order by payment desc")->fetchAll();
-        break;
-    }
-}else if(isset($_GET['pd'])){
-    $year=explode("-",$_GET['pd'])[0];
-    $period=explode("-",$_GET['pd'])[1];
-    $rows=$pdo->query("select * from `invoices` where period={$period}");
-}else if(isset($_GET['orderby'])){
+if(isset($_GET['orderby'])){
+    //收到orderby這個變數時，設定cookie的值，藉以用來判斷排序方式
     switch ($_GET['orderby']){
         case "date":
             $rows=$pdo->query("select * from `invoices` order by date desc")->fetchAll();
+            setcookie("order",'date',time()+3600);
         break;
         case "payment":
             $rows=$pdo->query("select * from `invoices` order by payment desc")->fetchAll();
+            setcookie("order",'payment',time()+3600);
         break;
     }
-}else{
+}else if(isset($_COOKIE['order'])){
+    //當cookie有設定時，以內容判斷排序方式
+        switch ($_COOKIE['order']){
+            case "date":
+                $rows=$pdo->query("select * from `invoices` order by date desc")->fetchAll();
+            break;
+            case "payment":
+                $rows=$pdo->query("select * from `invoices` order by payment desc")->fetchAll();
+            break;
+        }
+}else if(isset($_GET['pd'])){
+    //當網頁得到pd這個變數時，設定cookie儲存期數
+        $year=explode("-",$_GET['pd'])[0];
+        $period=explode("-",$_GET['pd'])[1];
+        $rows=$pdo->query("select * from `invoices` where period={$period}")->fetchAll();
+        setcookie("pd",$period,time()+3600);
+}else if(isset($_GET['pd'])&&isset($_COOKIE['order'])){
+    //當網頁得到pd以及order有儲存排序方式時
+        $year=explode("-",$_GET['pd'])[0];
+        $period=explode("-",$_GET['pd'])[1];
+        $orderby=$_COOKIE['order'];
+        echo "select * from `invoices` where period={$period}  order by {$orderby} desc";
+        $rows=$pdo->query("select * from `invoices` where period={$period}  order by {$orderby} desc")->fetchAll();
+}
+else{
+    //沒有收到變數時，找出全部的資料
     $rows=$pdo->query("select * from `invoices`")->fetchAll();
 }
+//以網址收到的參數判斷要顯示何時的發票以及依照甚麼排序
+// if(isset($_GET['pd'])&&isset($_GET['orderby'])){
+//     $year=explode("-",$_GET['pd'])[0];
+//     $period=explode("-",$_GET['pd'])[1];
+//     switch ($_GET['orderby']){
+//         case "date":
+//             $rows=$pdo->query("select * from `invoices` where period={$period} order by date desc")->fetchAll();
+//             setcookie("order",'date',time()+3600);
+//         break;
+//         case "payment":
+//             $rows=$pdo->query("select * from `invoices` where period={$period} order by payment desc")->fetchAll();
+//             setcookie("order",'payment',time()+3600);
+//         break;
+//     }
+// if(isset($_GET['pd'])){
+//     $year=explode("-",$_GET['pd'])[0];
+//     $period=explode("-",$_GET['pd'])[1];
+//     $rows=$pdo->query("select * from `invoices` where period={$period}");
+// }else if(isset($_COOKIE['order'])){
+//     switch ($_COOKIE['order']){
+//         case "date":
+//             $rows=$pdo->query("select * from `invoices` order by date desc")->fetchAll();
+//         break;
+//         case "payment":
+//             $rows=$pdo->query("select * from `invoices` order by payment desc")->fetchAll();
+//         break;
+//     }
+// }
+// else{
+//     $rows=$pdo->query("select * from `invoices`")->fetchAll();
+// }
 ?>
 <div class='row justify-content-around' style="list-style-type:none;padding:0">
-    <li><a href="?do=invoice_list&pd=2020-1">1,2月</a></li>
-    <li><a href="?do=invoice_list&pd=2020-2">3,4月</a></li>
-    <li><a href="?do=invoice_list&pd=2020-3">5,6月</a></li>
-    <li><a href="?do=invoice_list&pd=2020-4">7,8月</a></li>
-    <li><a href="?do=invoice_list&pd=2020-5">9,10月</a></li>
-    <li><a href="?do=invoice_list&pd=2020-6">11,12月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-1">1,2月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-2">3,4月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-3">5,6月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-4">7,8月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-5">9,10月</a></li>
+    <li><a href="?do=invoice_list&pd=<?=date('Y');?>-6">11,12月</a></li>
 
 </div>
 
@@ -95,7 +138,7 @@ if(isset($_GET['pd'])&&isset($_GET['orderby'])){
         }
     ?>
 </table>
-<ul class="pagination">
+<ul class="pagination mx-auto">
 <?php
     $pagecount=ceil(count($rows)/25);//判斷需要印出的列數
     
